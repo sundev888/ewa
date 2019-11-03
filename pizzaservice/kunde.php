4e9e314ce464be6cd0ee93d22ced5a4f5da9c348
Marwan B <marwan.bayoumi@gmail.com>
2019-11-03 12:51:34 +0100

    add background photo to other php files

--- pizzaservice/kunde.php
+++ pizzaservice/kunde.php
@@ -2,3 +2,17 @@
 include 'includes/header.php';
 ?>
 <link rel="stylesheet" href="main.css">
+
+<div>
+    <h3> Pizza Margherita</h3>
+    <p>In Zustellung</p>
+</div>
+
+<div>
+    <h3> Pizza Salami</h3>
+    <p>Im Offen</p>
+</div>
+
+<form action="">
+    <input type="button" value="Neue Bestellung">
+</form>
